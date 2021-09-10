b7708490b65eabf4d747f71535476707eec43f71
kukinpower <60896644+kukinpower@users.noreply.github.com>
2021-09-10 17:25:31 +0300

    Feature/add team container (#2)
    * add mock list team, and mock other commands
    
    * add batch and add one teammate
    
    * add remove, choose randomly implementation
    
    * temporary comment remove command

--- src/main/java/com/romankukin/projects/whoisondutybot/config/AppConfiguration.java
+++ src/main/java/com/romankukin/projects/whoisondutybot/config/AppConfiguration.java
@@ -1,10 +1,7 @@
 package com.romankukin.projects.whoisondutybot.config;
 
-import com.romankukin.projects.whoisondutybot.properties.AppProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 
 @Configuration
 @RequiredArgsConstructor
