179a8f7da32c732a07ec9bad7f4a36466d8eb597
kukinpower <thekukinpower@gmail.com>
2021-09-14 23:03:18 +0300

    add scheduled ping task

--- src/main/java/com/romankukin/projects/whoisondutybot/WhoIsOnDutyBotApplication.java
+++ src/main/java/com/romankukin/projects/whoisondutybot/WhoIsOnDutyBotApplication.java
@@ -2,8 +2,10 @@ package com.romankukin.projects.whoisondutybot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WhoIsOnDutyBotApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/romankukin/projects/whoisondutybot/scheduled/PreventIdlingErrorTask.java
@@ -0,0 +1,33 @@
+package com.romankukin.projects.whoisondutybot.scheduled;
+
+import com.romankukin.projects.whoisondutybot.properties.PingTaskProperties;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PreventIdlingErrorTask {
+
+  private final PingTaskProperties pingTaskProperties;
+
+  @Scheduled(fixedRateString = "#{@pingTaskProperties.getPeriod()}")
+  public void ping() {
+    try {
+      URL url = new URL(pingTaskProperties.getUrl());
+      HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+      httpURLConnection.connect();
+      log.info("Ping {}", url.getHost());
+      log.info("Response: {}", httpURLConnection.getResponseCode());
+      httpURLConnection.disconnect();
+    } catch (IOException e) {
+      log.error("Ping Failed");
+      e.printStackTrace();
+    }
+  }
+}
