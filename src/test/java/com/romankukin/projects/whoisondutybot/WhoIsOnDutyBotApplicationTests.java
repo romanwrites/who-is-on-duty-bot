c8c23ea9beb94ed7fcefc6fcf785fd02d6e32c97
kukinpower <thekukinpower@gmail.com>
2021-09-25 20:49:46 +0300

    add health test, split some strings to easier debug NPE

--- src/test/java/com/romankukin/projects/whoisondutybot/WhoIsOnDutyBotApplicationTests.java
+++ src/test/java/com/romankukin/projects/whoisondutybot/WhoIsOnDutyBotApplicationTests.java
@@ -1,13 +1,32 @@
 package com.romankukin.projects.whoisondutybot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import java.io.BufferedReader;
+import java.io.FileReader;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class WhoIsOnDutyBotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	void testHealth() throws Exception {
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.get("/health")
+				.accept(MediaType.APPLICATION_JSON);
+
+		mockMvc.perform(request).andExpect(status().isOk());
+	}
 }
